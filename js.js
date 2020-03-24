188c73b570d2601db484c659ef9d0039551a0cca
rbasa <40749051+rbasa@users.noreply.github.com>
2020-03-24 17:02:37 -0300

    correccion contacto
    se le añaden los attr name al form

---
+++ js.js
@@ -0,0 +1,27 @@
+// $(function() {
+//   $('#navbarEmpresas').hover(
+//     function() {
+//       $(this).color('red')
+//     },
+//     function() {
+//       $(this).color('blue')
+//     }
+//   )
+// })
+
+$('#linkEmpresas').hover(
+  function() {
+    $('#desplegableEmpresas').css('display', 'inherit')
+  },
+  function() {
+    $('#desplegableEmpresas').css('display', 'none')
+  }
+)
+$('#desplegableEmpresas').hover(
+  function() {
+    $('#desplegableEmpresas').css('display', 'inherit')
+  },
+  function() {
+    $('#desplegableEmpresas').css('display', 'none')
+  }
+)
