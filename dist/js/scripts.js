3b02cf2afe3010f2409d621f832b13980d3c7370
David Miller <hello@davidmiller.io>
2021-06-04 14:10:26 -0400

    bump version

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Heroic Features v4.3.0 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - Heroic Features v5.0.0 (https://startbootstrap.com/template/heroic-features)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
 */
