2d5cf9218bdf1b1bdf57ec641e53bb637725bc09
David Miller <hello@davidmiller.io>
2021-10-18 18:57:24 -0400

    Bootstrap 5.1.3 dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Heroic Features v5.0.3 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - Heroic Features v5.0.4 (https://startbootstrap.com/template/heroic-features)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
 */
