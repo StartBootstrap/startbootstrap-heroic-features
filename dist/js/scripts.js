abb77a9fe17d31ec79736f907771b75dbcc9077a
David Miller <hello@davidmiller.io>
2022-03-22 16:35:49 -0400

    dependencies update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Heroic Features v5.0.4 (https://startbootstrap.com/template/heroic-features)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
 */
 // This file is intentionally blank
